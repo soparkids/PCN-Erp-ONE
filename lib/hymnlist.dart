8f4452261578a7eb696d61e7ecb1e00ea2137511
okenwa <okenwaikwan@gmail.com>
2020-11-03 19:37:49 +0100

    one signal

--- lib/hymnlist.dart
+++ lib/hymnlist.dart
@@ -2,15 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rchmain/hymndetail.dart';
+import 'hymnsearch.dart';
 //import 'package:firebase_core/firebase_core.dart' show Firebase;
 
-class RchApp extends StatelessWidget {
+class RchApp extends StatefulWidget {
+  @override
+  _RchAppState createState() => _RchAppState();
+}
+
+class _RchAppState extends State<RchApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(" Presbyterian RCH"),
+          title: Text(" Presbyterian RCH "),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                    context: context,
+                    delegate: HymnSearchDelegate(messages: Query));
+              },
+            ),
+          ],
         ),
+        drawer: Drawer(
+            child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                      image: AssetImage("assets/rchhymn.jpg"),
+                      fit: BoxFit.cover)),
+              child: Container(),
+            ),
+            ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  Navigator.pop(context);
+                }),
+            ListTile(
+              title: Text("Favorites"),
+              leading: Icon(Icons.favorite),
+              onTap: () {
+                Navigator.pop(context);
+                // _displayFavoritesPage(context);
+              },
+            ),
+            ListTile(
+                title: Text("SSS Hymns"),
+                leading: Icon(Icons.book),
+                onTap: () {
+                  Navigator.pop(context);
+                  // _displayLanguagesPage(context);
+                }),
+            ListTile(
+              title: Text("Settings"),
+              leading: Icon(Icons.settings),
+              onTap: () {
+                Navigator.pop(context);
+                //_displaySettingsPage(context);
+              },
+            ),
+          ],
+        )),
         body: StreamBuilder<QuerySnapshot>(
             stream: FirebaseFirestore.instance.collection("rch").snapshots(),
             builder: (context, snapshot) {
@@ -34,7 +93,7 @@ class RchApp extends StatelessWidget {
                       isThreeLine: true,
                       subtitle: Text(
                         rch.data()['c4lyrics'],
-                        maxLines: 4,
+                        maxLines: 5,
                         overflow: TextOverflow.ellipsis,
                       ),
                       onTap: () {

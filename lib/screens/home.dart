58ffc7e5773ee971c7513ee9c735c16186403d3a
okenwa <okenwaikwan@gmail.com>
2021-09-05 06:01:13 +0100

    PCN nav class

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:pcn_erp/routes/routes.dart';
+//import 'package:pcn_erp/routes/routes.dart';
+import 'package:pcn_erp/nav/nav.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
@@ -54,86 +55,8 @@ class _MyHomePageState extends State<MyHomePage> {
           centerTitle: true,
           
         ),
-        
-        drawer: Drawer(
-            child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  image: DecorationImage(
-                      image: AssetImage("assets/pcnlogo1.jpg"),
-                      fit: BoxFit.cover)),
-              child: Container(),
-            ),
-            ListTile(
-                title: Text("Home"),
-                leading: Icon(Icons.home),
-                onTap: () {
-                  Navigator.pop(context);
-                }),
-            ListTile(
-              title: Text("Moderator of General Assembly"),
-              leading: Icon(Icons.person),
-              onTap: () {
-                Navigator.pop(context);
-                // _displayFavoritesPage(context);
-              },
-            ),
-            ListTile(
-                title: Text("PC of General Assembly"),
-                leading: Icon(Icons.person),
-                onTap: () {
-                  Navigator.pop(context);
-                  // _displayLanguagesPage(context);
-                }),
-            ListTile(
-                title: Text("Hymnal"),
-                leading: Icon(Icons.menu_book),
-                onTap: () {
-                  Navigator.pop(context);
-                  // _displayLanguagesPage(context);
-                }),
-            ListTile(
-                title: Text("Bible"),
-                leading: Icon(Icons.auto_stories_rounded),
-                onTap: () {
-                  Navigator.pop(context);
-                  // _displayLanguagesPage(context);
-                }),
-            ListTile(
-                title: Text("Practice & Procedure"),
-                leading: Icon(Icons.bookmark_add),
-                onTap: () {
-                  Navigator.pop(context);
-                  // _displayLanguagesPage(context);
-                }),
-            ListTile(
-                title: Text("The Blue Book"),
-                leading: Icon(Icons.bookmark_add),
-                onTap: () {
-                  Navigator.pop(context);
-                  // _displayLanguagesPage(context);
-                }),
-            ListTile(
-                title: Text("Approved Dates 2021"),
-                leading: Icon(Icons.calendar_today),
-                onTap: () {
-                  Navigator.pop(context);
-                  // _displayLanguagesPage(context);
-                }),
-          
-            ListTile(
-              title: Text("Settings"),
-              leading: Icon(Icons.settings),
-              onTap: () {
-                Navigator.pop(context);
-                //_displaySettingsPage(context);
-              },
-            ),
-          ],
-        )),
+          drawer: Navbar(),
+         
         body: Padding(
             padding: const EdgeInsets.only(top: 12.0),
             child: StaggeredGridView.count(
